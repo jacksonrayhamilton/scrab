3e0f3fe669510430d25430446f2efec765296704
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2014-07-07 09:21:51 -0700

    add prepositions

--- test/test.js
+++ test/test.js
@@ -3,31 +3,37 @@
 var scrab = require('../lib/scrab.js'),
     i;
 
-for (i = 0; i < 3; i++) {
-    console.log(scrab.getRandomWord());
-}
+// for (i = 0; i < 3; i++) {
+//     console.log(scrab.getRandomWord());
+// }
 
-for (i = 0; i < 3; i++) {
-    console.log(scrab.getRandomWord({
-        properize: true
-    }));
-}
+// for (i = 0; i < 3; i++) {
+//     console.log(scrab.getRandomWord({
+//         properize: true
+//     }));
+// }
 
-for (i = 0; i < 3; i++) {
-    console.log(scrab.getRandomSentence());
-}
+// for (i = 0; i < 3; i++) {
+//     console.log(scrab.getRandomSentence());
+// }
 
-for (i = 0; i < 3; i++) {
+// for (i = 0; i < 3; i++) {
+//     console.log(scrab.getRandomSentence({
+//         min: 2,
+//         max: 3,
+//         punctuate: false,
+//         properize: true
+//     }));
+// }
+
+// for (i = 0; i < 3; i++) {
+//     console.log(scrab.getRandomSentence({
+//         separate: true
+//     }));
+// }
+
+for (i = 0; i < 5; i++) {
     console.log(scrab.getRandomSentence({
-        min: 2,
-        max: 3,
-        punctuate: false,
-        properize: true
-    }));
-}
-
-for (i = 0; i < 3; i++) {
-    console.log(scrab.getRandomSentence({
-        separate: true
+        prepositions: true
     }));
 }
